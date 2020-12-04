1751edaba0d7ee14d5d588ed9e8a4724bdeb89e4
Saravanan raghul <saravananraghul1820@gmail.com>
2020-12-04 23:11:34 +0530

    Learnt JSX wat is Class-based & Function-based

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Person from './Person/Person';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<p>This is Awesome dude!!!!!!!!</p>
+				<Person />
+			</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,6 @@
+import React from 'react';
+// using Es6
+const person = () => {
+	return <h2>'HI This is valid person!!'</h2>;
+};
+export default person;
