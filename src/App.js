9f07a6d49f78fa3f5cb546958972a69ace3b1172
Saravanan raghul <saravananraghul1820@gmail.com>
2020-12-16 21:10:17 +0530

    Toggle List using ternary operator

--- src/App.js
+++ src/App.js
@@ -3,58 +3,63 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-
-
 class App extends Component {
 	state = {
-		persons:[
-			{name:"Sarvi", age: 20},
-			{name:"Saro", age: 19},
-			{name:"Baymax", age: 20},
-		]
-	}
+		persons: [{ name: 'Sarvi', age: 20 }, { name: 'Saro', age: 19 }, { name: 'Baymax', age: 20 }], showState: false
+	};
 	/*property tat can be executed as func. 
-	we r changing the state of person names here through btn click event listener*/ 
-	switchNameHandler = (newName) =>{
+	we r changing the state of person names here through btn click event listener*/
+
+	switchNameHandler = (newName) => {
 		// console.log("Btn check!!")
 		// setState method is provided by react to change the state of component
 		this.setState({
-			persons:[
-				{name:newName, age: 21},
-				{name:"Saravanan M", age: 20},
-				{name:"Baymax Alpha", age: 21},
-			]
-		}) 
-	}
+			persons: [{ name: newName, age: 21 }, { name: 'Saravanan M', age: 20 }, { name: 'Baymax Alpha', age: 21 }]
+		});
+	};
 
 	onNameChanged = (event) => {
 		this.setState({
-			persons:[
-				{name:"Saravanan V", age: 21},
-				{name:event.target.value, age: 20},
-				{name:"Baymax Alpha", age: 21},
+			persons: [
+				{ name: 'Saravanan V', age: 21 },
+				{ name: event.target.value, age: 20 },
+				{ name: 'Baymax Alpha', age: 21 }
 			]
-		}) 
+		});
+	};
+	toggleState = () => {
+		const isShow = this.state.showState;
+		this.setState({ showState: !isShow })
 	}
 	render() {
 		const btnStyle = {
-			backgroundColor:'white',
-			font:'inherit',
-			border:'3px solid blue',
-			padding:'8px'
-		}
+			backgroundColor: 'white',
+			font: 'inherit',
+			border: '3px solid blue',
+			padding: '8px'
+		};
 		return (
 			<div className="App">
-			{/* <header className="App-header"> */}
+				{/* <header className="App-header"> */}
 				{/* <img src={logo} className="App-logo" alt="logo" /> */}
 				<p>This is Awesome dude!!!!!!!!</p>
-				<button style={btnStyle} onClick={this.switchNameHandler.bind(this, 'Saravanan Alpha')}>Switch Name</button>
-				<Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
-				<Person name = {this.state.persons[1].name} age = {this.state.persons[1].age} changed = {this.onNameChanged}/>
-				<Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} />
-			{/* </header> */}
-		</div>
-		)
+				<button style={btnStyle} onClick={this.toggleState}>
+					Switch Name
+				</button>
+				{ this.state.showState ?
+					<div>
+						<Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+						<Person
+							name={this.state.persons[1].name}
+							age={this.state.persons[1].age}
+							changed={this.onNameChanged}
+						/>
+						<Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+					</div> : null
+				}
+				{/* </header> */}
+			</div>
+		);
 	}
 }
 
@@ -75,7 +80,7 @@ export default App;
 // 				{name:"Saravanan M", age: 20},
 // 				{name:"Baymax Alpha", age: 21},
 // 			]
-// 		}) 
+// 		})
 // 	}
 // 	return (
 // 		<div className="App">
@@ -92,11 +97,3 @@ export default App;
 // }
 
 // export default App;
-
-
-
-
-
-
-
-
