7c98accc3aa76c569a53cce67c6cdb07a71c7bb7
Saravanan raghul <saravananraghul1820@gmail.com>
2020-12-24 22:23:10 +0530

    Scoped styles for person.js

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,28 +1,16 @@
 import React from 'react';
-// import './Person.css';
-import styled from 'styled-components';
+import classes from './Person.module.css';
 
-const StyledDiv = styled.div`
-		width: 60%;
-		margin: 16px auto;
-		border: 1px solid #000;
-		padding: 16px;
-		box-shadow: 0 2px 3px #ccc;
-		text-align: center;
-		@media (min-width: 500px) {
-			width: 450px;
-		}
-`;
 // using Es6 not class based
 const person = (props) => {
 	return (
-		<StyledDiv>
+		<div className={classes.Person}>
 			<h2 onClick={props.click}>
 				I'm {props.name} and {props.age} years old
 			</h2>
 			<p>{props.children}</p>
 			<input type="text" onChange={props.changed} value={props.name} />
-		</StyledDiv>
+		</div>
 	);
 };
 export default person;
