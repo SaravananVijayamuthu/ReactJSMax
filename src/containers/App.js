918b62431d1f94aa8efb6bd2150f4d3832c922af
Saravanan raghul <saravananraghul1820@gmail.com>
2020-12-29 00:55:20 +0530

    Components and containers till cockpit

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import classes from './App.module.css';
 import Persons from '../components/Persons/Persons';
-
+import Cockpit from '../components/Cockpit/Cockit';
 class App extends Component {
 	state = {
 		persons: [
@@ -40,35 +40,19 @@ class App extends Component {
 
 	render() {
 		let persons = null;
-		let btnclasses = [classes.button];
 		if (this.state.showState) {
-			persons = (
-				<div>
-					<Persons 
-						persons = {this.state.persons}
-						delete = {this.deletePersonHandler}
-						change = {this.onNameChanged}
+			persons = 
+					<Persons
+						persons={this.state.persons}
+						delete={this.deletePersonHandler}
+						change={this.onNameChanged}
 					/>
-				</div>
-			);
-			btnclasses.push(classes.Red);
 		}
-
-		const assignedClasses = [];
-		if (this.state.persons.length <= 2) {
-			assignedClasses.push(classes.red);
-		}
-		if (this.state.persons.length <= 1) {
-			assignedClasses.push(classes.bold);
-		}
-
 		return (
 			<div className={classes.App}>
 				{/* <header className="App-header"> */}
 				{/* <img src={logo} className="App-logo" alt="logo" /> */}
-				<h1>Hi, I'm a React App</h1>
-				<p className={assignedClasses.join(' ')}>This is Working!!</p>
-				<button className={btnclasses.join(' ')} onClick={this.toggleState}>Show Persons</button>
+				<Cockpit showState ={this.state.showState} persons={this.state.persons} clicked = {this.toggleState}/>
 				{persons}
 				{/* </header> */}
 			</div>
